e7c1aa433402c088e5809c22a6219658bc73b913
Dhruv <dhruvramani98@gmail.com>
2019-11-15 06:06:08 +0530

    SAC ready

---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+
+def str2bool(v):
+    return v.lower() == 'true'
+
+def str2list(v):
+    if not v:
+        return v
+    else:
+        return [v_ for v_ in v.split(',')]
+
+def argparser():
+    parser = argparse.ArgumentParser("SORB for Continuous Action Space",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    
+    # ---- SAC ----
+    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
+    parser.add_argument('--hid', type=int, default=300)
+    parser.add_argument('--l', type=int, default=1)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--seed', '-s', type=int, default=0)
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--exp_name', type=str, default='half_cheetah_sac')
+    parser.add_argument('--save_dir', type=str, default='./models/')
+
+    # Policy Testing
+    parser.add_argument('--test_sac', type=str2bool, default='True')
+    parser.add_argument('--test_len', '-l', type=int, default=0)
+    parser.add_argument('--test_episodes', '-n', type=int, default=100)
+    parser.add_argument('--test_render', '-nr', type=str2bool, default='True')
+    parser.add_argument('--test_itr', '-i', type=int, default=-1)
+    parser.add_argument('--test_deterministic', '-d', action='store_true')
+
+    args = parser.parse_args()
+    return args
---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import gym
+import numpy as np
+
+import sac.core as core
+from sac.sac import sac
+from config import argparser
+from sac.utils.run_utils import setup_logger_kwargs
+from sac.utils.test_policy import load_policy, run_policy
+
+def main(config):
+	
+	path = os.path.join(config.save_dir, config.exp_name)
+	logger_kwargs = setup_logger_kwargs(config.exp_name, config.seed, config.save_dir)
+	
+	if(not os.path.isdir(path)):
+		sac(lambda : gym.make(config.env), actor_critic=core.mlp_actor_critic,
+	 		ac_kwargs=dict(hidden_sizes=[config.hid] * config.l),
+	 		gamma=config.gamma, seed=config.seed, epochs=config.epochs,
+	 		logger_kwargs=logger_kwargs)
+
+	if(config.test_sac):
+		env, get_action = load_policy(path, 
+	                                  config.test_itr if config.test_itr >= 0 else 'last',
+	                                  config.test_deterministic)
+		run_policy(env, get_action, config.test_len, config.test_episodes, config.test_render)
+
+
+if __name__ == '__main__':
+	config = argparser()
+	main(config)	
